ecb296c211a268aa23cd17a3c85a9014fb2152eb
Robert Hill <roberthill@kidazzle.com>
2026-04-12 13:50:21 -0400

    Update API_BASE to bypass localtunnel cache block

--- page-iro-console.php
+++ page-iro-console.php
@@ -45,7 +45,7 @@
 
     <script type="text/babel">
         const { useState, useEffect, useRef } = React;
-        const API_BASE = 'https://iro-bullmight-bridge14.loca.lt';
+        const API_BASE = 'https://iro-bullmight-bridge16.loca.lt';
         
         // Hardcore Dedicated Tunnels (Ensure they use the accessible remote tunnel URL, not localhost which fails in browser)
         const TUNNELS = {
