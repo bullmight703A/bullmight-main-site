4c5de270c20b6e947a2d9fad2e87ed273c4a3dfa
Robert Hill <roberthill@kidazzle.com>
2026-05-09 08:15:14 -0400

    Auto-deploy: Update IRO Bridge Cloudflare Tunnel

--- page-iro-console.php
+++ page-iro-console.php
@@ -45,7 +45,7 @@
 
     <script type="text/babel">
         const { useState, useEffect, useRef } = React;
-        const API_BASE = 'https://lynn-experiencing-rise-bulk.trycloudflare.com';
+        const API_BASE = 'https://bible-gdp-slow-hardcover.trycloudflare.com';
         
         // Hardcore Dedicated Tunnels (Replace API_BASE with HTTPS Cloudflare tunnel URLs when bound to ports)
         const TUNNELS = {
