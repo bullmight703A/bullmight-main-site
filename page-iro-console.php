fc15edfeb62da5bd3d5488e999bfebd08d9a7eda
Robert Hill <roberthill@kidazzle.com>
2026-06-12 08:15:12 -0400

    Auto-deploy: Update IRO Bridge Cloudflare Tunnel

--- page-iro-console.php
+++ page-iro-console.php
@@ -45,7 +45,7 @@
 
     <script type="text/babel">
         const { useState, useEffect, useRef } = React;
-        const API_BASE = 'https://anne-sheffield-watt-about.trycloudflare.com';
+        const API_BASE = 'https://anonymous-medicine-instructional-television.trycloudflare.com';
         
         // Hardcore Dedicated Tunnels (Replace API_BASE with HTTPS Cloudflare tunnel URLs when bound to ports)
         const TUNNELS = {
